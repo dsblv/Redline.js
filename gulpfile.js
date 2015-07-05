0a13075f41365a4064b3989872ac4ba580b1d2ff
Dmtry Sobolev <disobolev@icloud.com>
2015-07-05 22:27:54 +0300

    demo is static again

--- gulpfile.js
+++ gulpfile.js
@@ -16,16 +16,13 @@ var path = {
     source : {
         script : 'src/js/redline.js',
         style  : 'src/scss/redline.scss',
-        html   : 'src/html/redline.html',
-        demo   : 'src/demo/'
+        html   : 'src/html/redline.html'
     },
     watch  : {
         script : 'src/js/**/*',
         style  : 'src/scss/**/*',
-        html   : 'src/html/**/*',
-        demo   : 'src/demo'
-    },
-    demo   : 'demo/'
+        html   : 'src/html/**/*'
+    }
 };
 
 
@@ -66,18 +63,6 @@ gulp.task('html:build', function () {
 
 // Task that builds everything
 gulp.task('build', ['script:build', 'style:build']);
-
-// Task that compiles demo 
-gulp.task('demo:build', ['build'], function () {
-
-    gulp.src(path.source.demo + 'index.html')
-        .pipe(rigger())
-        .pipe(gulp.dest(path.demo));
-
-    gulp.src(path.source.demo + 'style.css')
-        .pipe(gulp.dest(path.demo));
-
-});
 
 // Task that cleans up the mess
 gulp.task('clean', function (cb) {
